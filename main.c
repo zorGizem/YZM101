7cf2f0186eca9f8ea60203e55645b63e9ff10389
zorGizem <117214387+zorGizem@users.noreply.github.com>
2022-11-03 09:52:30 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi;
+    printf("Bir sayi giriniz  ");
+    scanf("%d",&sayi);
+
+
+
+
+    if(sayi%2==0)
+    {
+        printf(" sayi cifttir\n");
+
+    }
+    else
+    {
+        printf("sayi tektir\n");
+    }
+
+
+    return 0;
+}
